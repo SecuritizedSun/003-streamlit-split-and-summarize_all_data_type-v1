b77f0e685f877dac6bcf6bad7537790e32a2cf18
SecuritizedSun <78744408+SecuritizedSun@users.noreply.github.com>
2025-08-12 22:39:18 -0400

    update

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 # ---------- Helper: LLM loader ----------
 def load_LLM(openai_api_key: str):
     # deterministic output - set temperature to 0
-    llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+    llm = OpenAI(temperature=0, openai_api_key=openai_api_key, model = "gpt-4o-mini")
     return llm
 
 # ---------- Streamlit UI ----------
